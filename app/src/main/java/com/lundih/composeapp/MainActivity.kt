ef0f73fa2e2dd2f99706c63beff029c589e2724e
lundih <lundih27@gmail.com>
2021-11-20 19:14:26 +0300

    Test out composables

--- app/src/main/java/com/lundih/composeapp/MainActivity.kt
+++ app/src/main/java/com/lundih/composeapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.lundih.composeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -17,7 +18,7 @@ class MainActivity : ComponentActivity() {
             ComposeAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    AppUI()
                 }
             }
         }
@@ -25,14 +26,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun AppUI () {
+    Column {
+        Name("Diana Prince")
+        Name("Bruce Wayne")
+        Name("John Stewart")
+        Name("Harlene Quinzel")
+    }
+}
+
+@Composable
+fun Name(name: String) {
+    Text(text = name)
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeAppTheme {
-        Greeting("Android")
+        AppUI()
     }
 }
